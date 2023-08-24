bb7b1bfe911a88f353a418bd2398b883f1a6d381
Avimitin <dev@avimit.in>
2023-08-25 04:30:40 +0800

    Initialize
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flakes for OCaml development";
+
+  inputs = {
+    # The nixpkgs
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Utility functions
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs.ocamlPackages;
+            [
+              ocaml
+              ocaml-lsp
+              utop
+            ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
